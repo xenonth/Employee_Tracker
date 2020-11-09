6eccf2015bd816f5ed6715e28701f275460386ac
Timothy Forbes <timothy.forbes259@gmail.com>
2020-11-09 17:02:39 +1030

    npm installation

---
+++ db/employees.sql
@@ -0,0 +1,15 @@
+DATABASE employee-management IF NOT EXISTS 
+CREATE DATABASE employee-management IF NOT EXISTS ,
+
+CREATE TABLE department (
+id INT AUTO_INCREMENT NOT NULL ,
+name VARCHAR(30) NOT NULL ,
+PRIMARY KEY(id) ,
+) ,
+
+CREATE TABLE role (
+	id INT AUTO_INCREMENT NOT NULL ,
+    title VARCHAR(30) , 
+    PRIMARY KEY(id) ,
+)
+ 
