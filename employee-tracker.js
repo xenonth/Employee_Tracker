dc8ec877f7c919b63fab7603a6b8720678d1fe6b
Timothy Forbes <timothy.forbes259@gmail.com>
2020-11-09 17:08:47 +1030

    ready to code

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,1 +1,10 @@
+const inquirer = require("inquirer");
+
 const count = 0;
+inquirer.prompt({
+    name: "response",
+    message: "who are you? ",
+    type: "input",
+}).then(function (anwers) {
+    console.log(anwers.response)
+})
