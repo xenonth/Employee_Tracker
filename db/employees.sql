57f82ff2b59131030143a0472b7397ff87854e45
Timothy Forbes <timothy.forbes259@gmail.com>
2020-11-09 17:22:12 +1030

    databae has been setup

--- db/employees.sql
+++ db/employees.sql
@@ -2,14 +2,24 @@ DATABASE employee-management IF NOT EXISTS
 CREATE DATABASE employee-management IF NOT EXISTS ,
 
 CREATE TABLE department (
-id INT AUTO_INCREMENT NOT NULL ,
-name VARCHAR(30) NOT NULL ,
-PRIMARY KEY(id) ,
+	id INT AUTO_INCREMENT NOT NULL ,
+	name VARCHAR(30) NOT NULL ,
+	PRIMARY KEY(id) ,
 ) ,
 
-CREATE TABLE role (
+CREATE TABLE job-duty (
 	id INT AUTO_INCREMENT NOT NULL ,
-    title VARCHAR(30) , 
+    title VARCHAR(30) NOT NULL ,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id FOREIGN KEY (nameId) REFERENCES (department) NOT NULL ,
     PRIMARY KEY(id) ,
+)
+
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT NOT NULL ,
+    first_name VARCHAR(30) NOT NULL ,
+    last_name VARCHAR(30) NOT NULL ,
+    role_id	FOREiGN KEY (job-dutyId) REFERENCES (job-duty)  NOT NULL ,
+    PRIMARY KEY(id) 
 )
  
